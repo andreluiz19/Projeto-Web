25fe22054a8a816e969e55b752386419279c31dd
andreluiz19 <andreavancini@alunos.utfpr.edu.br>
2022-05-03 15:28:55 -0300

    Andre

--- login.js
+++ login.js
@@ -6,3 +6,18 @@ bt.addEventListener("click", fnEntrar);
 function fnEntrar(){
     form.style.display = 'block';
 }
+
+let btf = document.getElementById('fechar');
+
+btf.addEventListener("click", fnFechar);
+
+function fnFechar(){
+    form.style.display = 'none';
+}
+
+
+fetch('https://reqres.in/api/login')
+.then(response => response.json())
+.then(json =>{
+    console.log(json.data);
+})
