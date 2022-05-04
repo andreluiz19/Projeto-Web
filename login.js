9e3ea7c6d5c5a32d2706fff9feec320751fc3e28
andreluiz19 <andreavancini@alunos.utfpr.edu.br>
2022-05-04 17:51:49 -0300

    Andre

--- login.js
+++ login.js
@@ -15,19 +15,45 @@ function fnFechar(){
     form.style.display = 'none';
 }
 
+let formu = document.getElementById('formLogin');
+let email = document.getElementById('user');
+let senha = document.getElementById('senha');
+
+formu.addEventListener('submit', function(e) {
+    axios.post('https://reqres.in/api/login', {
+        email: email.value,
+        password: senha.value
+    })
+    .then(function (response) {
+    console.log(response);
+    })
+    e.preventDefault();
+});
+
+
 /*
-fetch('https://reqres.in/api/login')
+let email = document.getElementById('user');
+let password = document.getElementById('senha');
+let credenciais = {email, password};
+let parametros = { method: 'POST' , body:  credenciais};
+
+fetch('https://reqres.in/api/login', parametros)
 .then(response => response.json())
 .then(json =>{
     console.log(json.data);
 })
 */
 
+
+
+
+
+/*
 let http = new XMLHttpRequest();
 http.open('POST', 'https://reqres.in/api/login', true);
 console.log(json.data);
 
-/*
+
 function validaLogin(){
     http.open('POST', 'https://reqres.in/api/login', true);
     console.log(json.data);
