7a65ba7a9408717404bf3a6c53cbe9db9dcc7405
andreluiz19 <andreavancini@alunos.utfpr.edu.br>
2022-05-03 22:04:06 -0300

    Andre

--- login.js
+++ login.js
@@ -15,9 +15,21 @@ function fnFechar(){
     form.style.display = 'none';
 }
 
-
+/*
 fetch('https://reqres.in/api/login')
 .then(response => response.json())
 .then(json =>{
     console.log(json.data);
 })
+*/
+
+let http = new XMLHttpRequest();
+http.open('POST', 'https://reqres.in/api/login', true);
+console.log(json.data);
+
+/*
+function validaLogin(){
+    http.open('POST', 'https://reqres.in/api/login', true);
+    console.log(json.data);
+}
+*/
